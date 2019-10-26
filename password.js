4d3aaf0746dcb8b0c56b98ce2c53556ae39265ce
Alex Stevens <zandra630@gmail.com>
2019-10-26 15:56:18 -0500

    Finalizes code

--- password.js
+++ password.js
@@ -1,9 +1,9 @@
-
 // Strings of our 4 possible character groups
 var upperStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerStr = "abcdefghijklmnopqrstuvwxyz";
 var specialStr =  " !#$%&()*+,-./:;<=>?@[\\]^_{|}~";
 var numStr = "1234567890";
+var passDisplay = document.querySelector("#password_display");
 
 // String to hold the possible characters the password can use
 var characters = "";
@@ -55,8 +55,8 @@ function generatePW() {
     alert("You must include at least one set of characters in your password.");
     return false;
   }
-  
   // We should now have our built character string 
+  console.log(password);
 
   // Now iterate through our character string the number of times chosen for pw length
   // On each iteration, we choose a random number, the random number is less than our character string length
@@ -67,32 +67,28 @@ function generatePW() {
     password += characters.charAt(x);
   }
 
-  console.log(password); // REMOVE THIS BEFORE SUBMITTING!!!
-
   // Display password on screen
-  function displayPW() {
-
-    // Create and style 'p' tag to hold our password string 
+  function displayPW(text) {
+    // Create and style an element to hold our password string 
     var pwInput = document.createElement("textarea");
+
     pwInput.setAttribute("id", "pwInput");
     pwInput.setAttribute("type", "text");
     pwInput.setAttribute("class", "form-control");
     pwInput.setAttribute("style", "margin: 10px");
-    // pwInput.setAttribute("width", "100%");
+    pwInput.setAttribute("readOnly", "true");
 
-    pwInput.value = password
-
-    // document.getElementById("my_input").value = password;
-    // ptag.setAttribute("style", "textAlign:center");
-    document.getElementById("password_display").appendChild(pwInput);
+    // Append textarea element to my #password_display div
+    passDisplay.appendChild(pwInput);
+    pwInput.value = text;
   }
+  displayPW(password);
 
-  displayPW();
-  // return password;
 }
+
 function copyClip() { 
   var pwText = document.getElementById("pwInput").select();
-  return pwText;
-
+  document.execCommand("copy");
+  alert("Copied the password: " + password);
+  location.reload();
 }
-
